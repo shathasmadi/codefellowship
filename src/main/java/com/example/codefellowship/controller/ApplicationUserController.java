d6e756189d8910cb4b60a16ede1a470e6597929c
shathasmadi <alsmadishatha@gmail.com>
2021-08-15 21:29:20 +0300

    lab 16 and 17

---
+++ src/main/java/com/example/codefellowship/controller/ApplicationUserController.java
@@ -0,0 +1,54 @@
+package com.example.codefellowship.controller;
+
+import com.example.codefellowship.model.ApplicationUser;
+import com.example.codefellowship.repository.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Controller
+public class ApplicationUserController {
+
+    @Autowired
+    ApplicationUserRepository  applicationUserRepository;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder   ;
+
+
+    @GetMapping("/login")
+    public String loginPage(){
+    return "signin";
+}
+
+    @GetMapping("/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+   @PostMapping("/signup")
+      public RedirectView signUp(@RequestParam(value ="username") String username,@RequestParam(value = "password") String password, @RequestParam(value = "firstName") String firstName, @RequestParam(value = "lastName") String lastName, @RequestParam(value = "dateOfBirth") String dateOfBirth,@RequestParam(value = "bio") String bio ){
+
+       ApplicationUser newUser = new ApplicationUser(username,bCryptPasswordEncoder.encode(password) , firstName, lastName, dateOfBirth,bio );
+       applicationUserRepository.save(newUser);
+       return  new RedirectView("/login");
+
+   }
+
+        @GetMapping("/users/{id}")
+          public String profilePage(Model modelOne, @PathVariable("id") int id){
+
+            modelOne.addAttribute("user",  applicationUserRepository.findById(id).get());
+            return "user";
+
+   }
+}
---
+++ src/main/java/com/example/codefellowship/model/ApplicationUser.java
@@ -0,0 +1,113 @@
+package com.example.codefellowship.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import javax.persistence.*;
+import java.util.Collection;
+
+
+@Entity
+public class ApplicationUser implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String bio;
+
+    public ApplicationUser() {
+
+    }
+
+    public ApplicationUser(String username, String password, String firstName, String lastName, String dateOfBirth,String bio) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.bio = bio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
