52379b7ba82b370f0f1b2a523f31d6a336a6f283
shathasmadi <alsmadishatha@gmail.com>
2021-08-15 15:57:44 +0300

    create a data base

---
+++ src/main/java/com/example/codefellowship/CodefellowshipApplication.java
@@ -0,0 +1,13 @@
+package com.example.codefellowship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodefellowshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodefellowshipApplication.class, args);
+	}
+
+}
